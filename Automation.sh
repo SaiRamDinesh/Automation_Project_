d9bf4bd6a13ecbd0d109134a5e17e914c3677077
SaiRamDinesh <122209745+SaiRamDinesh@users.noreply.github.com>
2023-01-10 09:22:44 +0530

    Update Automation.sh

--- Automation.sh
+++ Automation.sh
@@ -1,44 +1,42 @@
-#!/bin/bash
-
-# Variables
-name="tayyab"
-s3_bucket="your_bucket"
-
-# update the ubuntu repositories
-apt update -y
-
-# Check if apache2 is installed
-if [[ apache2 != $(dpkg --get-selections apache2 | awk '{print $1}') ]]; then
-	#statements
-	apt install apache2 -y
+install= dpkg --get-selections apache2 | awk '{print $2}'
+if [[ install != ${install} ]];
+then
+        sudo apt install apache2
+else
+        echo "apache2 package is already installed"
 fi
 
-# Ensures that apache2 service is running
-running=$(systemctl status apache2 | grep active | awk '{print $3}' | tr -d '()')
-if [[ running != ${running} ]]; then
-	#statements
-	systemctl start apache2
+
+
+#to check apache2 service status running
+
+status_check= (sudo systemctl status apache2 | grep running | awk '{print $3}' | tr -d '()')
+#if the apache2 is not running
+
+if [[ running != {$status_check} ]];
+then
+        sudo systemctl start apache2
+else
+        echo "apache2 service is already running"
 fi
 
-# Ensures apache2 Service is enabled 
-enabled=$(systemctl is-enabled apache2 | grep "enabled")
-if [[ enabled != ${enabled} ]]; then
-	#statements
-	systemctl enable apache2
+#to check apache2 service enabled
+enabled= (systemctl is-enabled apache2)
+if [[ enabled != ${enabled} ]];
+then
+        sudo systemctl enable apache2
+else
+        echo "apache2 service is already enabled"
 fi
-
-# Creating file name
+cd /var/log/apache2/
+name="Dinesh"
 timestamp=$(date '+%d%m%Y-%H%M%S')
-
-# Create tar archive of apache2 access and error logs
-cd /var/log/apache2
 tar -cf /tmp/${name}-httpd-logs-${timestamp}.tar *.log
 
-# copy logs to s3 bucket
-if [[ -f /tmp/${name}-httpd-logs-${timestamp}.tar ]]; then
-	#statements
-	aws s3 cp /tmp/${name}-httpd-logs-${timestamp}.tar s3://${s3_bucket}/${name}-httpd-logs-${timestamp}.tar
-fi
+s3_bucket="upgrad-dinesh"
+aws s3 \
+        cp /tmp/${name}-httpd-logs-${timestamp}.tar \
+        s3://${s3_bucket}/${name}-httpd-logs-${timestamp}.tar
 
 
 
